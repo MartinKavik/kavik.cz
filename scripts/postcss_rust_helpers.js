37f6b3e4e1fc1807be7376749e04aa381d13f758
Martin Kavík <martin.kavik@gmail.com>
2019-09-06 00:57:31 +0200

    extract css classes also from index.html

--- scripts/postcss_rust_helpers.js
+++ scripts/postcss_rust_helpers.js
@@ -5,43 +5,18 @@ const fs = require('fs');
 // Helpers are used by configs/postcss.config.js
 
 module.exports = {
+    escapeClassName: escapeClassName,
+
     /**
-     * Search in Rust files for C.class_name
+     * Search in Rust files and index.html for CSS classes
      *
-     * @returns usedCssClasses Set of class names
+     * @returns Set of class names
      */
     getUsedCssClasses: function() {
-        const usedCssClasses = new Set;
-        // search in Rust files
-        const files = findFiles.fileSync(/\.rs$/, './crate/src');
-        files.forEach(filePath => {
-            const fileContent = fs.readFileSync(filePath, 'utf8')
-            // example of a used class in Rust code is `C.mb_16`
-            usedCssClassesInFile = fileContent.match(/C\.[a-z0-9_]+/g)
-            if(usedCssClassesInFile) {
-                usedCssClassesInFile
-                    // remove prefix `C.`
-                    .map(class_ => class_.substring(2))
-                    .forEach(class_ => usedCssClasses.add(class_))
-            }
-        })
-        return usedCssClasses
-    },
-    /**
-     * Transform css class name into indentifier that can be used as Rust struct field name
-     * (see https://github.com/MartinKavik/postcss-typed-css-classes/blob/master/generators/rust_generator.js)
-     *
-     * @param {string} name
-     * @returns string Escaped class name
-     */
-    escapeClassName: function (name) {
-        name = name.replace(/-/g, '_')
-        name = name.replace(/:/g, '__')
-        name = name.replace(/\//g, 'of')
-        if (getKeywords().indexOf(name) > -1) {
-            name += '_'
-        }
-        return name
+        return new Set([
+            ...getUsedCssClassesInRust(),
+            ...getUsedCssClassesInIndex()
+        ])
     }
 }
 
@@ -100,4 +75,69 @@ function getKeywords () {
         'virtual',
         'yield'
     ]
+}
+
+/**
+ * Search in Rust files for C.class_name
+ *
+ * @returns usedCssClasses Set of class names
+ */
+function getUsedCssClassesInRust() {
+    const usedCssClasses = new Set;
+    // search in Rust files
+    const files = findFiles.fileSync(/\.rs$/, './crate/src');
+    files.forEach(filePath => {
+        const fileContent = fs.readFileSync(filePath, 'utf8')
+        // example of a used class in Rust code is `C.mb_16`
+        const usedCssClassesInFile = fileContent.match(/C\.[a-z0-9_]+/g) || []
+        usedCssClassesInFile
+            // remove prefix `C.`
+            .map(class_ => class_.substring(2))
+            // add class to set
+            .forEach(class_ => usedCssClasses.add(class_))
+    })
+    return usedCssClasses
+}
+
+/**
+ * Search in index.html for CSS classes
+ *
+ * @returns usedCssClasses Set of class names
+ */
+function getUsedCssClassesInIndex() {
+    const usedCssClasses = new Set;
+    const fileContent = fs.readFileSync('./entries/index.html', 'utf8')
+    // example of a used class in HTML code is `class="mb-16 text-blue-1"` or `class='font-bold'`
+    // Note: We cannot use capturing group because of `/g` (and stateful regexes are ugly)
+    const classAttribute = fileContent.match(/class\s*=\s*['|"][^'|"]+['|"]/g) || []
+    classAttribute
+        // extract classes from from attribute (i.e. class="one two" => "one two"
+        .map(classAttribute => classAttribute.match(/class\s*=\s*['|"]([^'|"]+)['|"]/)[1])
+        .forEach(content => {
+            // split classes
+            const classes = content.match(/\S+/g) || []
+                classes
+                    // transform classes to Rust form
+                    .map(escapeClassName)
+                    // add class to set
+                    .forEach(class_ => usedCssClasses.add(class_))
+        })
+    return usedCssClasses
+}
+
+/**
+ * Transform css class name into indentifier that can be used as Rust struct field name
+ * (see https://github.com/MartinKavik/postcss-typed-css-classes/blob/master/generators/rust_generator.js)
+ *
+ * @param {string} name
+ * @returns string Escaped class name
+ */
+function escapeClassName(name) {
+    name = name.replace(/-/g, '_')
+    name = name.replace(/:/g, '__')
+    name = name.replace(/\//g, 'of')
+    if (getKeywords().indexOf(name) > -1) {
+        name += '_'
+    }
+    return name
 }
